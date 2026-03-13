620e1a81a90170d316e5d4b5f20b16e69851ccc3
veenaputhenveedu123-droid <veenaputhenveedu123@gmail.com>
2026-03-13 18:35:28 +0530

    Update urls.py

--- vendor_app/urls.py
+++ vendor_app/urls.py
@@ -6,6 +6,6 @@ path('api/register/', RegisterView.as_view(), name='register'),
 path('api/login/', vendor_loginview, name='login'),
 path('api/products/', VendorProductListView.as_view()),
 path('api/products/add/', ProductCreateView.as_view()),
-path('api/products/<int:pk>/', ProductUpdateView.as_view()),
-path('api/products/<int:pk>/', ProductDeleteView.as_view()),
+path('api/products/<int:pk>/update/', ProductUpdateView.as_view()),
+path('api/products/<int:pk>/delete/', ProductDeleteView.as_view()),
 ]
